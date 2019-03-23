10820292a00eac0796d64162e35a861fdc63d0ca
Mats Gustafsson <mats@mcnab.se>
2019-03-23 09:46:09 +0100

    adds dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((company-clang-arguments . ("-I/home/mats/projects/SoapySDR_TXRX_Burst_Tester/SoapySDR_TXRX_Burst_Tester/classes/sdr/"
+                                     "-I/home/mats/projects/SoapySDR_TXRX_Burst_Tester/SoapySDR_TXRX_Burst_Tester/classes/utils/")))))
